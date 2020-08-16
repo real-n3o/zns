08732b7c784fd603a0f9b4c0ae4745d994f413af
real-n3o <c1-02@countermail.com>
2020-08-16 13:07:27 +0200

    first proto of ZNS

---
+++ Contracts/HandleRegistry.sol
@@ -0,0 +1,61 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+/**
+ * @title Registry
+ * @dev Store & retrieve registered ZNS Handle entries
+*/
+
+contract HandleRegistry {
+
+    struct HandleRegistration {
+        address owner;
+        string handle;
+        string zId;
+    }
+
+    mapping (string => HandleRegistration) handleMap;
+    HandleRegistration[] public handles;
+
+    function registerHandle(address _owner, string memory _handle, string memory _zId) public {
+        require(zIdCheck(_zId));
+        
+        HandleRegistration storage registration = handleMap[_handle];
+        
+        registration.owner = _owner;
+        registration.handle = _handle; 
+        registration.zId = _zId;
+        
+        handles.push(registration);
+    }
+ 
+    function zIdCheck(string memory _zId) public pure returns (bool){
+        if(stringsEqual(_zId, "0x36BA68c5B715405A373C5C7aBCda625E25F39175")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function getHandleAddress (string memory _handle) public view returns (address) {
+        HandleRegistration memory registration = handleMap[_handle];
+        return registration.owner;
+    }
+    
+    function stringsEqual(string memory _a, string memory _b) internal pure returns (bool) {
+        bytes memory a = bytes(_a);
+        bytes memory b = bytes(_b);
+        if(bytes(a).length==bytes(b).length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+}
+
+// >> to_do:
+
+// 0. check if entry already exists
+// 1. check if zId is valid
+// 2. ? 
+// 3. charge xx Infinity in order to enter ... 
