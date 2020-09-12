5496e966233a834e71db21a26ebfe78119a24b1a
real-n3o <n1-02@countermail.com>
2020-09-12 13:21:22 -0700

    more param definitions on contracts

--- contracts/Registrar.sol
+++ contracts/Registrar.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.6.0;
 
 /**
  * @title Registrar
- * @dev Create and interact with Registries
+ * @dev Create and interact with Registries.
 */
 
 import './Registry.sol';
@@ -11,6 +11,7 @@ import './RegistryController.sol';
 
 contract Registrar { 
 
+    /// @notice Emitted when a new Registry is added to the Registrar.
     event registryAdded(address registry, string _domain, string registryType);
 
     struct RegistrarEntry {
@@ -24,6 +25,15 @@ contract Registrar {
 
     mapping (string => RegistrarEntry) registryMap;
     address[] public registrar;
+
+    /**
+     * @notice Creates a new Registry and associated Entry in the Registrar.
+     * @param _domain The primary name used to represent a particular Registry. 
+     * @param _ref The primary reference that a Registry links to. This can be any type of content and is typically used to reference an index or url.
+     * @param _registryType The Registry's Type. Registry Types are defined globally at the Registrar and used to categorize Registries.
+     * @param _stakePrice The amount of Infinity Token required to register a new Entry in the Registry. 
+     * @param _registryToken The address of the Registry's Token. Registry Tokens are issued upon staking Infinity Token to transfer the ownership of a Registry Entry.
+     */
 
     function createRegistry (
         string memory _domain,
@@ -51,13 +61,27 @@ contract Registrar {
         emit registryAdded(registryMap[_domain].registry, _domain, _registryType);
     }
 
+    /**
+     * @notice Returns the the number of Registries in the Registrar as an interger. 
+     */
+
     function getRegistrarLength() public view returns (uint256) {
         return registrar.length;
     }
 
+    /**
+     * @notice Returns the address of a Registry by providing its domain as a string.
+     * @param _domain The primary name used to represent a particular Registry. 
+     */
+
     function getRegistryAddress(string memory _domain) public view returns (address) {
         return registryMap[_domain].registry;
     }
+
+    /**
+     * @notice Returns the Type of a Registry by providing its domain as a string.
+     * @param _domain The primary name used to represent a particular Registry. 
+     */
 
     function getRegistryType(string memory _domain) public view returns (string memory) {
         return registryMap[_domain].registryType;
--- contracts/RegistryController.sol
+++ contracts/RegistryController.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.4.22 <0.7.0;
 
 /**
  * @title RegistryController
- * @dev Create and interact with ZNS
+ * @dev Create and interact with ZNS.
 */
 
 import './Registry.sol';
@@ -17,19 +17,19 @@ contract RegistryController {
     address payable public registryTokenAddress;
     uint256 public stakePrice;
 
-    /// @notice Emitted when a new registry is created
+    /// @notice Emitted when a new Registry is created.
     event createdRegistry(address registrtyAddress, address registryTokenAddress);
 
-    /// @notice Emitted when a registry's stake price is updated
+    /// @notice Emitted when a Registry's stake price is updated.
     event stakePriceSet(uint256 newStakePrice);
 
-    /// @notice Emitted when a new entry is created in a specific a registry
+    /// @notice Emitted when a new entry is created in a Registry.
     event createdRegistryEntry(string subdomain, string ref);
 
-    /// @notice Emitted when a new a registry ref is updated
+    /// @notice Emitted when a new a Registry 'ref' is updated.
     event registryRefUpdated(string newRef);
 
-    /// @notice Emitted when a new a registry entry ref is updated
+    /// @notice Emitted when a new a Registry Entry 'ref' is updated.
     event registryEntryRefUpdated(string subdomain, string newRef);
 
     function init(address _registry, address payable _registryToken) 
@@ -42,8 +42,8 @@ contract RegistryController {
     }
 
     /**
-     * @notice Sets a new stake price for a registries stake token
-     * @param _newStakePrice The new stake price to be used by the registry
+     * @notice Sets a new stake price for a Registry's RegistryToken.
+     * @param _newStakePrice The new stake price to be used by the registry.
      */
 
     function setStakePrice(uint256 _newStakePrice)
@@ -56,9 +56,9 @@ contract RegistryController {
     }
 
     /**
-     * @notice Creates a new registry entry within a specific registry
-     * @param _subdomain The subdomain that will be created within the registry entry
-     * @param _ref The user-defined reference (such as a domain) that the subdomain will point to
+     * @notice Creates a new entry within a Registry.
+     * @param _subdomain The subdomain that will be utilized to reference the current Registry entry.
+     * @param _ref The user-defined reference (such as an index or url) that the Registry entry will point to.
      */
 
     function createRegistryEntry(
@@ -77,8 +77,8 @@ contract RegistryController {
     }
 
     /**
-     * @notice Updates a registry's ref value
-     * @param _newRef The new ref value to be updated to
+     * @notice Updates a Registry's referece value.
+     * @param _newRef The new reference value to be updated to.
      */
 
     function setRegistryRef(
@@ -92,9 +92,9 @@ contract RegistryController {
     }
 
     /**
-     * @notice Updates a registry entries ref value
-     * @param _subdomain The subdomain of the registry entry to be updated
-     * @param _newRef The new ref value to be updated in the registry entry
+     * @notice Updates a Registry entries reference value.
+     * @param _subdomain The subdomain of the Registry entry to be updated.
+     * @param _newRef The new reference value to be updated in the Registry entry.
      */
 
     function setRegistryEntryRef(
